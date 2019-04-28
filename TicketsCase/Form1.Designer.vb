35bbed0c14d74fb8243a05164674542d77d3ae88
Matt Sim <mr.matt.sim@gmail.com>
2019-04-28 13:20:16 -0400

    Add project files.

---
+++ TicketsCase/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTickets
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnPrize = New System.Windows.Forms.Button()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.txtTickets = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnPrize
+        '
+        Me.btnPrize.Location = New System.Drawing.Point(28, 98)
+        Me.btnPrize.Name = "btnPrize"
+        Me.btnPrize.Size = New System.Drawing.Size(208, 23)
+        Me.btnPrize.TabIndex = 0
+        Me.btnPrize.Text = "Find me a cool prize!"
+        Me.btnPrize.UseVisualStyleBackColor = True
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Location = New System.Drawing.Point(30, 23)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(208, 17)
+        Me.lblInstructions.TabIndex = 1
+        Me.lblInstructions.Text = "How many tickets did you earn?"
+        '
+        'txtTickets
+        '
+        Me.txtTickets.Location = New System.Drawing.Point(28, 56)
+        Me.txtTickets.Name = "txtTickets"
+        Me.txtTickets.Size = New System.Drawing.Size(208, 22)
+        Me.txtTickets.TabIndex = 2
+        '
+        'frmTickets
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(265, 145)
+        Me.Controls.Add(Me.txtTickets)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Controls.Add(Me.btnPrize)
+        Me.Name = "frmTickets"
+        Me.Text = "Prizes!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnPrize As Button
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents txtTickets As TextBox
+End Class
---
+++ TicketsCase/Form1.vb
@@ -0,0 +1,39 @@
+﻿'Name: Raven Sim
+'Date: March 14th, 2019
+'Description: Tells you what prizes you can have based on the tickets you won.
+
+Public Class frmTickets
+
+    'VARIABLES
+    'How many tickets they have
+    Dim tickets As Integer
+
+
+    Private Sub btnPrize_Click(sender As Object, e As EventArgs) Handles btnPrize.Click
+
+        'check if the textbox is numeric
+        If IsNumeric(txtTickets.Text) Then
+            'assign the number as the tickets they've earned
+            tickets = txtTickets.Text
+
+            'select case for different ranges of tickets and the prizes they can earn 
+            Select Case tickets
+                Case 0 To 9
+                    MsgBox("You don't have enough tickets for a prize. Go play some more games and come back later!", , "Not yet...")
+                Case 10 To 49
+                    MsgBox("You can choose a keychain, bag of chips, mood ring or bouncy ball!", , "Small prize")
+                Case 50 To 99
+                    MsgBox("You can choose a teddy bear, goldfish, ring pop, bag of marbles or a printed t-shirt!", , "Medium prize")
+                Case 100 To 999
+                    MsgBox("You can choose a video game, a slice of pizza, a water bottle or an ice cream cone!", , "Large Prize")
+                Case Is >= 100
+                    MsgBox("Congrats! You can choose an iPad, a laptop or an ice cube tray!", , "Crazy prize!")
+            End Select
+
+        Else
+            'if it is not numeric, show an error message
+            MsgBox("Please enter a valid number.", , "Error")
+        End If
+
+    End Sub
+End Class
